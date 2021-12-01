062d9d05759925cf8668c1d90dfc87c9cb1bff97
“[negar <miralaienegar@gmail.com>
2021-12-02 01:23:09 +0330

    added countries and styled them

---
+++ script/app.js
@@ -0,0 +1,14 @@
+// const searchInput = document.getElementById("search");
+// searchInput.addEventListener("keyup", event => {
+//     searchInputValue = searchInput.value;
+//     const liElements= document.getElementsByTagName("li");
+
+//     liElements.forEach(liElement => {
+//         if(liElement.innerHTML.toLowerCase().includes(searchInputValue)){
+//             liElement.style.display = "";
+//         }else{
+//             liElement.style.display = "none";
+//         }
+//     });
+
+// })
