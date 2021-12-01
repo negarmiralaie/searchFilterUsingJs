8c97677c6dd53b1111f8797f84ba76436e15f610
“[negar <miralaienegar@gmail.com>
2021-12-02 01:49:42 +0330

    completed js code =)

--- script/app.js
+++ script/app.js
@@ -1,14 +1,17 @@
-// const searchInput = document.getElementById("search");
-// searchInput.addEventListener("keyup", event => {
-//     searchInputValue = searchInput.value;
-//     const liElements= document.getElementsByTagName("li");
+const searchInput = document.getElementById("search");
+const liElements = document.getElementsByTagName("li");
+searchInput.addEventListener("keyup", event => {
+    searchInputValue = searchInput.value;
+    const liElements = document.getElementsByTagName("li");
+    console.log(liElements);
 
-//     liElements.forEach(liElement => {
-//         if(liElement.innerHTML.toLowerCase().includes(searchInputValue)){
-//             liElement.style.display = "";
-//         }else{
-//             liElement.style.display = "none";
-//         }
-//     });
+    for (let i = 0; i < liElements.length; i++) {
+        console.log(liElements[i]);
+        if (liElements[i].innerHTML.toLowerCase().includes(searchInputValue)) {
+            liElements[i].style.display = "";
+        } else {
+            liElements[i].style.display = "none";
+        }
+    }
 
-// })
+})
